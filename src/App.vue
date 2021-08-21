b91a774bfcd44883ffbf4dbe2a7a91ca18c77204
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2021-08-21 12:21:42 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ 
+ <div>
+   <todos></todos>
+ </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="todoapp">
+
+        <header>
+            <h1>
+                Todos
+            </h1>
+
+            <input type="text" class="new-todo" placeholder="add task" />
+        </header>
+
+        <div class="main">
+            <ul class="todo-list">
+                <li class="todo">
+                    <div class="view">
+                        <label>
+                            Task name
+                        </label>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+
+<script>
+
+export default {
+
+
+}
+
+</script>
+
+
+<style src="./todos.css">
+
+</style>
